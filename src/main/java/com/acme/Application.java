5f3f5f5045aab51f083af9fe138aad9b6e6d638d
Bruno Araujo <bdraraujo@gmail.com>
2016-04-12 22:46:41 -0400

    Adding AppEngine maven plugin

--- src/main/java/com/acme/Application.java
+++ src/main/java/com/acme/Application.java
@@ -1,5 +1,6 @@
 package com.acme;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
@@ -15,4 +16,7 @@ public class Application extends SpringBootServletInitializer {
         return application.sources(Application.class);
     }
 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 }

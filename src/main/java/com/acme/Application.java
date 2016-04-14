969986242efea6daff9478b3c4183ae75f4c738c
Bruno Araujo <bdraraujo@gmail.com>
2016-04-13 20:47:14 -0400

    Modified configuration for AppEngine compatibility (Servlet 2.5) and few changes to have application running standalone

--- src/main/java/com/acme/Application.java
+++ src/main/java/com/acme/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 /**
  * Created by bdraraujo on 16-04-12.
@@ -17,6 +20,14 @@ public class Application extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
 }

880c16e616f8986e75b70e272ce65e4674052054
Bruno Araujo <bdraraujo@gmail.com>
2016-04-14 23:58:29 -0400

    Test of signing using OAuth

--- src/main/java/com/acme/rest/NotifyController.java
+++ src/main/java/com/acme/rest/NotifyController.java
@@ -2,15 +2,17 @@ package com.acme.rest;
 
 import com.acme.types.NotifyResponse;
 import com.google.api.client.auth.oauth.OAuthHmacSigner;
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.basic.DefaultOAuthConsumer;
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+import oauth.signpost.signature.QueryStringSigningStrategy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.security.GeneralSecurityException;
-import java.sql.Timestamp;
-import java.util.Date;
 
 /**
  * Created by bdraraujo on 16-04-12.
@@ -20,7 +22,8 @@ public class NotifyController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/notify")
     public NotifyResponse notify(@RequestParam(value = "url") String url) {
-        final String key = "product-1-105571";
+        final String consumerKey = "product-1-105571";
+        final String secret = "CzHCBcYLAWMxpKJl";
         // lPcbzdKCj8CJ5ZN9
         // TODO Call AppDirect with signed URL to get XML back
         OAuthHmacSigner signer = new OAuthHmacSigner();
@@ -30,14 +33,33 @@ public class NotifyController {
         try {
             //destUrl = new URL(url);
             //HttpURLConnection request = (HttpURLConnection) destUrl.openConnection();
-            String signature = signer.computeSignature(url); //&oauth_nonce=95009478
-            String template = "&oauth_timestamp=%d&oauth_consumer_key=%s&oauth_signature_method=HMAC-SHA1&oauth_version=1.0&oauth_signature=%s";
-            String finalUrl = url + String.format(template, new Timestamp((new Date()).getTime()).getTime(), key, signature);
-            System.out.println(finalUrl);
+            /*String signature = signer.computeSignature(url);
+            //String template = "&oauth_timestamp=%d&oauth_consumer_key=%s&oauth_signature_method=HMAC-SHA1&oauth_version=1.0&oauth_signature=%s";
+            OAuthParameters params = new OAuthParameters();
+            params.setOAuthConsumerKey(consumerKey);
+            params.setOAuthNonce(OAuthUtil.getNonce());
+            params.setOAuthTimestamp(OAuthUtil.getTimestamp());
+            params.setOAuthSignatureMethod(signer.getSignatureMethod());
+            params.setOAuthType(OAuthParameters.OAuthType.TWO_LEGGED_OAUTH);
+            params.setOAuthSignature(signature);
+            params.addCustomBaseParameter("oauth_version", "1.0");
+
+            //signer.setPrivateKey(privKey);
+            //String finalUrl = url + String.format(template, new Timestamp((new Date()).getTime()).getTime(), key, signature);
+            String baseString = OAuthUtil.encode(OAuthUtil.normalizeUrl(url)) + '&'
+                    + OAuthUtil.encode(OAuthUtil.normalizeParameters(url, params.getBaseParameters()));*/
+            OAuthConsumer consumer = new DefaultOAuthConsumer(consumerKey, secret);
+            consumer.setSigningStrategy(new QueryStringSigningStrategy());
+            String signedUrl = consumer.sign(url);
+            System.out.println(signedUrl);
             RestTemplate restTemplate = new RestTemplate();
-            String response = restTemplate.getForObject(finalUrl, String.class, (Object) null);
+            String response = restTemplate.getForObject(signedUrl, String.class, (Object) null);
             System.out.println(response);
-        } catch (GeneralSecurityException e) {
+        } catch (OAuthExpectationFailedException e) {
+            e.printStackTrace();
+        } catch (OAuthMessageSignerException e) {
+            e.printStackTrace();
+        } catch (OAuthCommunicationException e) {
             e.printStackTrace();
         }
 

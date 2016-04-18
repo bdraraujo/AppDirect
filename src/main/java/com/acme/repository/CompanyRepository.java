df7c5a23bbd1bf081f3ff6b8e1eabf1ca17e3844
Bruno Araujo <bdraraujo@gmail.com>
2016-04-18 19:31:21 -0400

    Added mutiple repositories

--- src/main/java/com/acme/repository/CompanyRepository.java
+++ src/main/java/com/acme/repository/CompanyRepository.java
@@ -3,9 +3,11 @@ package com.acme.repository;
 import com.acme.types.Company;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by bdraraujo on 16-04-18.
  */
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, String> {
 }
